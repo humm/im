6959bd6d4a23251519c6312b3fa973a1421a1203
dell-company <10806623@qq.com>
2021-04-17 23:30:13 +0800

    图形界面工具初始化

---
+++ im-tool-ui/src/main/java/com/hoomoomoo/im/ImToolUiApplication.java
@@ -0,0 +1,13 @@
+package com.hoomoomoo.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImToolUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImToolUiApplication.class, args);
+    }
+
+}
---
+++ im-tool-ui/src/test/java/com/hoomoomoo/im/ImToolUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoomoomoo.im;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImToolUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
