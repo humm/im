abdce2544e85a75c8022c4e8b5e09b605dc66fbf
unknown <10806623@qq.com>
2025-01-22 15:46:38 +0800

    .

--- im-tool-ui/im-tool-ui-base/src/main/java/com/hoomoomoo/im/consts/MenuFunctionConfig.java
+++ im-tool-ui/im-tool-ui-base/src/main/java/com/hoomoomoo/im/consts/MenuFunctionConfig.java
@@ -10,13 +10,12 @@ public class MenuFunctionConfig {
 
     public enum MenuConfig {
 
-        // TA菜单
-        MENU_SVN("menuSvn", "SVN", 10, "svn"),
-        MENU_SCRIPT("menuCommon", "基础", 20, "common"),
-        MENU_SYSTEM("menuSystem", "高级", 50, "system"),
-
-        // 公共菜单
-        MENU_SET("menuSet", "设置", 1000, "set");
+        MENU_REPOSITORY("menuRepository", "仓库", 100, "repository"),
+        MENU_SCRIPT("menuScript", "脚本", 200, "script"),
+        MENU_FILE("menuFile", "文件", 300, "file"),
+        MENU_HEP("menuHep", "效能", 400, "hep"),
+        MENU_SET("menuSet", "设置", 500, "set"),
+        MENU_HELP("menuHelp", "帮助", 600, "help");
 
         private String menuId;
 
@@ -61,47 +60,51 @@ public class MenuFunctionConfig {
 
     public enum FunctionConfig {
         // TA工具组件 0-999
-        SVN_LOG("100", "提交记录", "/conf/fxml/svnLog.fxml", "svnLog", "svnLog", "menuSvn", "用户信息,提交记录"),
+        SVN_LOG("100", "提交记录", "/conf/fxml/svnLog.fxml", "svnLog", "svnLog", "menuRepository", "用户信息,提交记录"),
 
-        SVN_UPDATE("110", "文件更新", "/conf/fxml/svnUpdate.fxml", "svnUpdate", "svnUpdate", "menuSvn", "用户信息,文件更新"),
+        SVN_UPDATE("110", "文件更新", "/conf/fxml/svnUpdate.fxml", "svnUpdate", "svnUpdate", "menuRepository", "用户信息,文件更新"),
 
-        SVN_REALTIME_STAT("120", "实时统计", "/conf/fxml/svnRealtimeStat.fxml", "svnRealtimeStat", "svnRealtimeStat", "menuSvn", "用户信息,统计用户,实时统计"),
+        SVN_REALTIME_STAT("120", "实时统计", "/conf/fxml/svnRealtimeStat.fxml", "svnRealtimeStat", "svnRealtimeStat", "menuRepository", "用户信息,统计用户,实时统计"),
 
-        SVN_HISTORY_STAT("130", "历史统计", "/conf/fxml/svnHistoryStat.fxml", "svnHistoryStat", "svnHistoryStat", "menuSvn", "用户信息,统计用户,历史统计"),
-
-        SCRIPT_UPDATE("200", "升级脚本", "/conf/fxml/scriptUpdate.fxml", "scriptUpdate", "scriptUpdate", "menuCommon", "升级脚本"),
-
-        PROCESS_INFO("210", "流程信息", "/conf/fxml/processInfo.fxml", "processInfo", "processInfo", "menuCommon", "流程信息"),
-
-        FUND_INFO("220", "基金信息", "/conf/fxml/fundInfo.fxml", "fundInfo", "fundInfo", "menuCommon", "基金信息"),
-
-        DATABASE_SCRIPT("230", "执行脚本", "/conf/fxml/databaseScript.fxml", "databaseScript", "databaseScript", "menuCommon", "执行脚本"),
-
-        GENERATE_SQL("240", "分库分表", "/conf/fxml/generateSql.fxml", "generateSql", "generateSql", "menuCommon", "分库分表"),
-
-        COPY_CODE("300", "复制文件", "/conf/fxml/copyCode.fxml", "copyCode", "copyCode", "menuCommon", "复制文件"),
-
-        //GENERATE_CODE("310", "生成代码", "/conf/fxml/generateCode.fxml", "generateCode", "generateCode", "menuCommon", "生成代码"),
-
-        TASK_TODO("400", "开发任务", "/conf/fxml/hepTaskTodo.fxml", "hepTaskTodo", "hepTaskTodo", "menuSystem", "开发任务"),
-
-        TASK_SYNC("410", "同步需求", "/conf/fxml/hepTaskSync.fxml", "hepTaskSync", "hepTaskSync", "menuSystem", "同步需求"),
-
-        FILE_SYNC("420", "同步文件", "/conf/fxml/fileSync.fxml", "fileSync", "fileSync", "menuSystem", "同步文件"),
-
-        SCRIPT_CHECK("500", "检查文件", "/conf/fxml/scriptCheck.fxml", "scriptCheck", "scriptCheck", "menuSystem", "检查文件"),
-
-        CHANGE_FUNCTION_TOOL("600", "版本切换", "/conf/fxml/changeFunctionTool.fxml", "changeFunctionTool", "changeFunctionTool", "menuSystem", "版本切换"),
+        SVN_HISTORY_STAT("130", "历史统计", "/conf/fxml/svnHistoryStat.fxml", "svnHistoryStat", "svnHistoryStat", "menuRepository", "用户信息,统计用户,历史统计"),
 
 
-        SYSTEM_TOOL("700", "系统工具", "/conf/fxml/systemTool.fxml", "systemTool", "systemTool", "menuSystem", "系统工具"),
+        SCRIPT_UPDATE("200", "升级脚本", "/conf/fxml/scriptUpdate.fxml", "scriptUpdate", "scriptUpdate", "menuScript", "升级脚本"),
+
+        PROCESS_INFO("210", "流程信息", "/conf/fxml/processInfo.fxml", "processInfo", "processInfo", "menuScript", "流程信息"),
+
+        FUND_INFO("220", "基金信息", "/conf/fxml/fundInfo.fxml", "fundInfo", "fundInfo", "menuScript", "基金信息"),
+
+        DATABASE_SCRIPT("230", "执行脚本", "/conf/fxml/databaseScript.fxml", "databaseScript", "databaseScript", "menuScript", "执行脚本"),
+
+        GENERATE_SQL("240", "分库分表", "/conf/fxml/generateSql.fxml", "generateSql", "generateSql", "menuScript", "分库分表"),
+
+
+        COPY_CODE("300", "复制文件", "/conf/fxml/copyCode.fxml", "copyCode", "copyCode", "menuFile", "复制文件"),
+
+        FILE_SYNC("420", "同步文件", "/conf/fxml/fileSync.fxml", "fileSync", "fileSync", "menuFile", "同步文件"),
+
+        SCRIPT_CHECK("500", "检查文件", "/conf/fxml/scriptCheck.fxml", "scriptCheck", "scriptCheck", "menuFile", "检查文件"),
+
+
+        //GENERATE_CODE("310", "生成代码", "/conf/fxml/generateCode.fxml", "generateCode", "generateCode", "menuFile", "生成代码"),
+
+        TASK_TODO("400", "开发任务", "/conf/fxml/hepTaskTodo.fxml", "hepTaskTodo", "hepTaskTodo", "menuHep", "开发任务"),
+
+        TASK_SYNC("410", "同步需求", "/conf/fxml/hepTaskSync.fxml", "hepTaskSync", "hepTaskSync", "menuHep", "同步需求"),
+
+        CHANGE_FUNCTION_TOOL("600", "版本切换", "/conf/fxml/changeFunctionTool.fxml", "changeFunctionTool", "changeFunctionTool", "menuHep", "版本切换"),
+
+
+        SYSTEM_TOOL("700", "系统工具", "/conf/fxml/systemTool.fxml", "systemTool", "systemTool", "menuSet", "系统工具"),
 
         // 公共组件 3000-9999
         CONFIG_SET("9999", "参数设置", "/conf/fxml/configSet.fxml", "configSet", "configSet", "menuSet", "应用信息"),
 
-        FUNCTION_STAT_INFO("3000", "使用统计", "/conf/fxml/functionStatInfo.fxml", "functionStatInfo", "functionStatInfo", "menuSet", ""),
 
-        ABOUT_INFO("3100", "关于", "/conf/fxml/aboutInfo.fxml", "aboutInfo", "aboutInfo", "menuSet", "");
+        FUNCTION_STAT_INFO("3000", "使用统计", "/conf/fxml/functionStatInfo.fxml", "functionStatInfo", "functionStatInfo", "menuHelp", ""),
+
+        ABOUT_INFO("3100", "关于", "/conf/fxml/aboutInfo.fxml", "aboutInfo", "aboutInfo", "menuHelp", "");
 
 
         private String code;
